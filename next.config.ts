dd0410f6a59172451be611f8099f4e0448d947da
Sudesh Bhadouria <sudesh.bhadouria@accenture.com>
2026-03-20 14:49:01 +0100

    prepare for vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
-const repoName = "sudesh-cert-portfolio";
-
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },

48b0c6f4705e5483e7e13c36be17a7ec4a417beb
Sudesh Bhadouria <sudesh.bhadouria@accenture.com>
2026-03-20 15:22:43 +0100

    UI upgrade - premium version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}", // 👈 add this
   ],
   theme: {
     extend: {},

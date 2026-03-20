622b0f87a22046ddc2e6660fa45d0d47ac43a109
Sudesh Bhadouria <sudesh.bhadouria@accenture.com>
2026-03-20 14:43:29 +0100

    enhancement commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-
 const repoName = "sudesh-cert-portfolio";
 
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },

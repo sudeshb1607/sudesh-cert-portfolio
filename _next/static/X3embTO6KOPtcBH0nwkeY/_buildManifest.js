829cbd41cc0289c39a152cc0d99c81da76652a8f
Sudesh Bhadouria <sudesh.bhadouria@accenture.com>
2026-03-20 14:46:55 +0100

    Updates

--- _next/static/X3embTO6KOPtcBH0nwkeY/_buildManifest.js
+++ _next/static/X3embTO6KOPtcBH0nwkeY/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/sudesh-cert-portfolio//_next/:path+",
-        "destination": "/sudesh-cert-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
